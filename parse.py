88f47d201f5bd0dbe65c512bc27f334d48bed7f0
Jarrett Baugh <jarrett.baugh@gmail.com>
2020-10-27 18:42:40 -0700

    Switch to Pillow

--- parse.py
+++ parse.py
@@ -1,4 +1,3 @@
-import cv2
 import os, sys
 from PIL import Image
 
@@ -12,28 +11,24 @@ def pixel_to_rgba(pixel):
   return hex_chunk(pixel[0]) + hex_chunk(pixel[1]) + hex_chunk(pixel[2]) + hex_chunk(pixel[3])
 
 
-def get_pixels(fpath, ignore_colors = []):
+def get_pixels(fpath, ignore_colors = [], ignore_invisible = True):
+  pixels = {}
   with Image.open(fpath) as im:
-  for i in range(1, im.size[0]):
-    for j in range(1, im.size[1]):
-      pixVal = im.getpixel((i, j))
-      print(pixVal)
+    for i in range(1, im.size[0]):
+      for j in range(1, im.size[1]):
+        pixel = im.getpixel((i, j))
 
-  # img = cv2.imread(fpath, cv2.IMREAD_UNCHANGED)
-  # pixels = {}
+        # If ignore_invisible is True, ignore pixels with alpha of 0 (i.e. invisible)
+        if not ignore_invisible or pixel[3] != 0:
+          hex_pixel = pixel_to_rgba(pixel)
+          if hex_pixel not in ignore_colors:
+            if hex_pixel in pixels:
+              pixels[hex_pixel] += 1
+            else:
+              pixels[hex_pixel] = 1
 
-  # for row in img:
-  #   for pixel in row:
-  #     if pixel[3] != 0:
-  #       hex_pixel = pixel_to_rgba(pixel)
-  #       if hex_pixel not in ignore_colors:
-  #         if hex_pixel in pixels:
-  #           pixels[hex_pixel] += 1
-  #         else:
-  #           pixels[hex_pixel] = 1
-
-  # # Sort the pixels
-  # return {k: v for k, v in reversed(sorted(pixels.items(), key=lambda item: item[1]))}
+  # Sort the pixels
+  return {k: v for k, v in reversed(sorted(pixels.items(), key=lambda item: item[1]))}
 
 
 def print_pixels(pixels):
@@ -44,6 +39,6 @@ def generate_image(pixels):
   cv2.imwrite("output/test.png", [pixels.values()])
 
 pixels = get_pixels('samples/sprite1.png')
-# print_pixels(pixels)
+print_pixels(pixels)
 # generate_image(pixels)
 
